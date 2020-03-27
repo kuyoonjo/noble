8820e13ae84bf63df7fdf13498ddee8c0c595a38
Yu Chen <yu.chen@live.ie>
2020-03-27 18:13:29 +0800

    path.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const os = require('os');
 
-const m = os.platform() === 'win32' ? 'noble-winrt' : 'noble-unix';
+const m = os.platform() === 'win32' ? '@kuyoonjo/noble-winrt' : '@kuyoonjo/noble-unix';
 module.exports = require(m);
