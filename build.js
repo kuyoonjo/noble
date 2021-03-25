f797937516bc090a207083c48d205a467269dbf8
Yu Chen <yu.chen@live.ie>
2021-03-25 09:45:09 +0800

    feat electron.

--- build.js
+++ build.js
@@ -3,15 +3,21 @@ const { execSync, spawnSync } = require('child_process');
 function darwin() {
   execSync('rm -rf unix/prebuilds');
   execSync('prebuild -t 15.11.0 -r node -p unix', { stdio: 'inherit' });
-  execSync(`mkdir -p build/darwin/${process.arch}`);
-  execSync(`mv unix/lib/mac/native/binding.node build/darwin/${process.arch}`);
+  execSync(`mkdir -p build/darwin/node/${process.arch}`);
+  execSync(`mv unix/lib/mac/native/binding.node build/darwin/node/${process.arch}`);
+  execSync('prebuild -t 12.0.2 -r electron -p unix', { stdio: 'inherit' });
+  execSync(`mkdir -p build/darwin/electron/${process.arch}`);
+  execSync(`mv unix/lib/mac/native/binding.node build/darwin/electron/${process.arch}`);
 }
 
 function win32() {
   spawnSync(process.env.comspec, ['/c', 'rmdir', '/s', '/q', 'winrt\\prebuilds'], { stdio: 'inherit' });
   execSync('prebuild -t 15.11.0 -r node -p winrt', { stdio: 'inherit' });
-  spawnSync(process.env.comspec, ['/c', 'mkdir', `build\\win32\\${process.arch}`]);
-  spawnSync(process.env.comspec, ['/c', 'move', 'winrt\\build\\Release\\noble_winrt.node', `build\\win32\\${process.arch}\\binding.node`]);
+  spawnSync(process.env.comspec, ['/c', 'mkdir', `build\\win32\\node\\${process.arch}`]);
+  spawnSync(process.env.comspec, ['/c', 'move', 'winrt\\build\\Release\\noble_winrt.node', `build\\win32\\node\\${process.arch}\\binding.node`]);
+  execSync('prebuild -t 12.0.2 -r electron -p winrt', { stdio: 'inherit' });
+  spawnSync(process.env.comspec, ['/c', 'mkdir', `build\\win32\\electron\\${process.arch}`]);
+  spawnSync(process.env.comspec, ['/c', 'move', 'winrt\\build\\Release\\noble_winrt.node', `build\\win32\\electron\\${process.arch}\\binding.node`]);
 }
 
 if (process.platform === 'darwin')
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const Noble = require('./unix/lib/noble');
 const { EventEmitter } = require('events');
 const { inherits } = require('util');
+const isElectron = require('is-electron');
+
+const type = isElectron() ? 'electron' : 'node';
 
 switch (process.platform) {
   case 'darwin':
-    const NobleMac = require(`./build/${process.platform}/${process.arch}/binding`).NobleMac;
+    const NobleMac = require(`./build/${process.platform}/${type}/${process.arch}/binding`).NobleMac;
     inherits(NobleMac, EventEmitter);
     module.exports = new Noble(new NobleMac());
     break;
   case 'win32':
-    const NobleWinrt = require(`./build/${process.platform}/${process.arch}/binding`).NobleWinrt;
+    const NobleWinrt = require(`./build/${process.platform}/${type}/${process.arch}/binding`).NobleWinrt;
     inherits(NobleWinrt, EventEmitter);
     module.exports = new Noble(new NobleWinrt());
     break;
